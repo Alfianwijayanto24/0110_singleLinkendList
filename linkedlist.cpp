6ed992d2e64f768c2ea5289c42ae802ec2be439b
Alfianwijayanto24 <alvianwijayanto74@gmail.com>
2025-05-08 15:44:16 +0700

    bool detNode nim

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -62,5 +62,16 @@ public:
         Node *current, *previous;
         if (!Search(nim, &previous, &current))
         return false;
+
+        if (current == START)
+        START = START->next;
+        else
+        previous->next = current->next;
+
+        delete current;
+        return true;
     }
+    void traveres()
+
+    
 }
