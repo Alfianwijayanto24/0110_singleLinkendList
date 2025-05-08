9f41148c79ef889a14a38b825cdd987d5beefd14
Alfianwijayanto24 <alvianwijayanto74@gmail.com>
2025-05-08 15:59:54 +0700

    membuat int main

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -91,4 +91,22 @@ public:
     }
 
     
+};
+int main()
+{
+    List mhs;
+    int nim;
+    char ch;
+    while(1);
+    {
+        cout<<"Menu"<< endl;
+        cout<< endl
+            << "1. Membuat data kedalam list" << endl;
+        cout<< "2. Menghapus data dari dalam list" << endl;
+        cout<< "3. Menampilkan semua data dalam list "<< endl;
+        cout<< "4. Mencari data dalam list" << endl;
+        cout<< "5. Keluar"<< endl;
+        cout<< "Masukan pilihan (1-5):"<< endl;
+
+    }
 }
